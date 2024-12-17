ec9a56cadddf646e3be3b18fa2b7233ac8bbf8c0
Natalija Konovalova <natalja.konovalova@gmail.com>
2024-12-17 17:45:48 +0200

    Final commit

--- Main.js
+++ Main.js
@@ -1,39 +1,40 @@
 const questions = [
     {
-        id: '1',
-        question: "What language works in browser?",
-        answers: ["Java", "C", "Python", "Javascript"],
-        correct: 4,
+        question: "Which planet in the solar system is known as the “Red Planet”?",
+        answers: ["Venus", "Earth", "Mars", "Jupiter"],
+        correct: 3,
     },
     {
-        id: '2',
-        question: "What does CSS mean?",
+        question: "What is the largest lake in the world?",
         answers: [
-            "Central Style Sheets",
-            "Cascading Style Sheets", 
-            "Cascading Simple Sheets", 
-            "Cars SUV Sailboats",
+            "Caspian Sea",
+            "Baikal",
+            "Lake Superior",
+            "Ontario",
         ],
         correct: 2,
     },
     {
-		id: '3',
-        question: "What does HTML mean?",
+        question: " Which river is the longest in the world?",
 		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
+			"Amazon",
+            "Mississippi",
+            "Nile",
+            "Yangtze",
 		],
-		correct: 1,
+		correct: 3,
 	},
 	{
-		id: '4',
-        question: "When was JavaScript created?",
-		answers: ["1996", "1995", "1994", "all answers are incorrect"],
-		correct: 2,
+        question: "What animal is the national symbol of Australia?",
+		answers: ["Kangaroo", "Koala", "Emu", "Crocodile"],
+		correct: 1,
 	},
-];
+    {
+        question: "Which is the largest island?",
+		answers: ["New Guinea", "Andaman Nicobar", "Greenland", "Hawaii"],
+		correct: 3,
+	}
+ ];
 
 
 //Elementu paieska
@@ -45,7 +46,7 @@ const submitBtn = document.querySelector('#submit');
 let score = 0; //teisingu atsakymu skaicius
 let questionIndex = 0; //einamasis klausimas
 
-clearPage();
+clearPage(); 
 showQuestion();
 submitBtn.onclick = checkAnswer;
 
@@ -63,24 +64,100 @@ function showQuestion() {
         headerContainer.innerHTML = title;
 
      //atsakymai
-    for (answerText of questions[questionIndex]['answers']) {
-        console.log(answerText);
 
+     let answerNumber = 1;
+    for (answerText of questions[questionIndex]['answers']) {
+        
         const questionTemplate = 
             `<li>
                 <label>
-                    <input type="radio" class="answer" name="answer" />
+                    <input value="%number%" type="radio" class="answer" name="answer" />
                     <span>%answers%</span>
                 </label>
              </li>`
 
-            const answerHTML = questionTemplate.replace('%answers%', answerText);
+            const answerHTML = questionTemplate
+                                    .replace('%answers%', answerText)
+                                    .replace('%number%', answerNumber);
+
             listContainer.innerHTML += answerHTML;
-            console.log(answerHTML);
+           answerNumber++;
+         
     }
 }
 
 function checkAnswer() {
     console.log('checkAnswer started!');
-}
+    const checkedRadio = listContainer.querySelector('input:checked');
+    
+//jeigu atsakymo nera, nieko nedarome, isejimas is funkcijos
+    if(!checkedRadio) {
+        submitBtn.blur();
+        return
+    }
+
+    //suzinoti vartotojo atsakymo numeri
+    const userAnswer = parseInt(checkedRadio.value);
+
+    //jeigu atsakymas teisingas - padidinti score
+    if (userAnswer === questions[questionIndex]['correct']) {
+score++;
+    }
+    console.log('score=', score);
+
+    if(questionIndex !== questions.length - 1) {
+        console.log('There are more questions ahead');
+        questionIndex++;
+        clearPage();
+        showQuestion();
+        return;
+    } else {
+        clearPage();
+        showResults();
+    }
+
+    function showResults() {
+        console.log('showResults started!');
+        const resultsTemplate = `
+            <h2 class="title">%title%</h2>
+            <h3 class="summary">%message%</h3>
+            <p class="result">%result%</p>
+        `;
+
+        let title, message;
+
+        //Finalo variantai
+    
+    if(score === questions.length) {
+        title = 'Congratulations!';
+        message = 'Your all answers are correct!';
+    } else if((score * 100) / questions.length >= 50) {
+        title = 'Good result!';
+        message = 'You answered more that half of answers!';
+    } else {
+        title = 'Let try more!';
+        message = 'You can do better result!';
+    }
+
+    //Resultatas
+
+    let result =  `${score} from ${questions.length}`;
+
+    // Finalinis atsakymas
+
+    const finalMessage = resultsTemplate
+    .replace('%title%', title)
+    .replace('%message%', message)
+    .replace('%result%', result)
+
+    headerContainer.innerHTML = finalMessage;
+
+    //keiciam migtuka 
+    submitBtn.blur();
+    submitBtn.innerHTML = 'Start over';
+    submitBtn.onclick = () => history.go();
+
+
+    }
+ }
 
